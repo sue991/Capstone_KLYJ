d951fc94ded85e130b6bba5c3139dfc3e37b6443
wogns0197 <wogns0197@naver.com>
2020-09-11 17:21:13 +0900

    2

--- practice/p1.js
+++ practice/p1.js
@@ -2,6 +2,7 @@ var clicked=true;
 function click_menu(){
     if(clicked){
         $(".menubar").css("transform","translateX(0px)");
+        $(".menubar").css("border-radius","0 0 300px 0");
         $(".pre_icon").css("transform","translateX(350px)");
         $(".post_icon").css("opacity","1");
         $(".pre_opacity").css("opacity","0");
@@ -9,13 +10,14 @@ function click_menu(){
         // 메뉴 위,아래 라인 가운데로 흡수효과
         $(".line--1").css(
             "transform","translateY(10px) rotate(30deg)");
-            $(".line--3").css(
-                "transform","translateY(-10px) rotate(-30deg)");
+        $(".line--3").css(
+            "transform","translateY(-10px) rotate(-30deg)");
         
         clicked=false;
     }
     else{
         $(".menubar").css("transform","translateX(-500px)");
+        $(".menubar").css("border-radius","0 0 0 0");
         $(".pre_icon").css("transform","translateX(0px)");
         $(".pre_opacity").css("opacity","1");
         
--- slide.js
+++ slide.js
@@ -1,33 +1,58 @@
-var lnum = -300;
+var lnum = -600;
 var clicknum=1;
+var clickmenu=true;
 var barnum = 0;
+var cardnum=2 
+var c = ".c";
+
+// var tmp = c+(cardnum-1);
+// var tmp2 = c+(cardnum+1);
+// $(tmp).css("opacity","0.2");
+// $(tmp2).css("opacity","0.2");
+
 
 function right_button() {   
+    
     var a = $(".card"); 
     var bar = $(".bar");
     if(barnum<=90){
-        lnum-=300;
+        cardnum++;
+        lnum-=800;
         barnum += 30;
         a.css("transform","translate("+lnum+"px,0)");
         bar.css("transform","translate("+barnum+"px");
+        var tmp = c+cardnum;
+        var tmp2 = c+(cardnum-1);
+        var tmp3 = c+(cardnum+1);
+        $(tmp).css("opacity","1");
+        $(tmp2).css("opacity","0.4");
+        $(tmp3).css("opacity","0.4");
+        $(tmp).css("box-shadow","0px 5px 10px 5px gray");
     }
-    
-    
     // var list = ["c1","c2","c3","c4","c5"];
     // for (var i=0; i<list.length; i++){
     //     var tmp = $("." + list[i]);
     //     tmp.css("transform","translate(-100px,0)");     
     // }
 }
-
-function left_button() {    
+function left_button() {   
+    
     var a = $(".card");
     var bar = $(".bar"); 
     if(barnum>=30){
-        lnum+=300;
+        cardnum-- 
+        lnum+=800;
         barnum-=30;
         a.css("transform","translate("+lnum+"px,0)");
         bar.css("transform","translate("+barnum+"px");
+        var tmp = c+cardnum;
+        var tmp2 = c+(cardnum-1);
+        var tmp3 = c+(cardnum+1);
+        $(tmp).css("opacity","1");
+        $(tmp2).css("opacity","0.4");
+        $(tmp3).css("opacity","0.4");
+        $(tmp).css("box-shadow","0px 5px 10px 5px gray");
+
     }
     
     // var list = ["c1","c2","c3","c4","c5"];
@@ -35,21 +60,30 @@ function left_button() {
     //     var tmp = $("." + list[i]);
     //     tmp.css("transform","translate(+100px,0)");
     // }
-
 }
+
 function clicked_search(){
     if(clicknum==1){
-        // $("#menu").css("transform","rotate(90deg)");
+        
         $(".popup").css("opacity","0.8");
         clicknum=2;
     }
     else{
         // $(".popup").css("z-index","-1");
         $(".popup").css("opacity","0");
-        // $("#menu").css("transform","rotate(0deg)");
+        
         clicknum=1;
     }
+}
+function clicked_menu(){
+    if(clickmenu){
+        $(".menu").css("transform","translateX(0)");
         
+        clickmenu=false;
+    }
+    else{
+        $(".menu").css("transform","translateX(100%)");
+        clickmenu=true;
+    }
     
 }
-// setInterval(left_button,3000);
