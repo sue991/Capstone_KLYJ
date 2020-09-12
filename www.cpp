7cc57cf42c54388abe7fdb56fadd523f810258b3
sue991 <sue991@naver.com>
2020-09-12 16:16:21 +0900

    terminal로

---
+++ www.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <stack>
+#include <vector>
+using namespace std;
+
+
+int main(){
+	int n;
+
+	vector <int> v; // 답
+	stack <int> stk; // 중간과정
+	vector <int> sol;
+	vector <char> r;
+
+	
+}
