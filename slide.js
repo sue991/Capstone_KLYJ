b75f11d827b00265bf42580ad07493ee2219e27f
wogns0197 <wogns0197@naver.com>
2020-09-13 18:24:39 +0900

    왜안돼ㅜ

--- slide.js
+++ slide.js
@@ -3,7 +3,6 @@ var clicknum=1;
 var clickmenu=true;
 var barnum = 0;
 var cardnum=2 
-var c = ".c";
 
 // var tmp = c+(cardnum-1);
 // var tmp2 = c+(cardnum+1);
@@ -21,13 +20,11 @@ function right_button() {
         barnum += 30;
         a.css("transform","translate("+lnum+"px,0)");
         bar.css("transform","translate("+barnum+"px");
-        var tmp = c+cardnum;
-        var tmp2 = c+(cardnum-1);
-        var tmp3 = c+(cardnum+1);
-        $(tmp).css("opacity","1");
-        $(tmp2).css("opacity","0.4");
-        $(tmp3).css("opacity","0.4");
-        $(tmp).css("box-shadow","0px 5px 10px 5px gray");
+        
+        $(".c"+cardnum).css("filter","brightness(100%)");
+        $(".c"+(cardnum-1)).css("filter","brightness(35%)");
+        $(".c"+(cardnum+1)).css("filter","brightness(35%)");
+        // $(tmp).css("box-shadow","0px 5px 10px 5px gray");
     }
     // var list = ["c1","c2","c3","c4","c5"];
     // for (var i=0; i<list.length; i++){
@@ -45,13 +42,11 @@ function left_button() {
         barnum-=30;
         a.css("transform","translate("+lnum+"px,0)");
         bar.css("transform","translate("+barnum+"px");
-        var tmp = c+cardnum;
-        var tmp2 = c+(cardnum-1);
-        var tmp3 = c+(cardnum+1);
-        $(tmp).css("opacity","1");
-        $(tmp2).css("opacity","0.4");
-        $(tmp3).css("opacity","0.4");
-        $(tmp).css("box-shadow","0px 5px 10px 5px gray");
+        
+        $(".c"+cardnum).css("filter","brightness(100%)");
+        $(".c"+(cardnum-1)).css("filter","brightness(35%)");
+        $(".c"+(cardnum+1)).css("filter","brightness(35%)");
+        // $(tmp).css("box-shadow","0px 5px 10px 5px gray");
 
     }
     
@@ -77,13 +72,31 @@ function clicked_search(){
 }
 function clicked_menu(){
     if(clickmenu){
-        $(".menu").css("transform","translateX(0)");
-        
         clickmenu=false;
+        $(".menu").css("transform","translateX(0)");
+        $(".menuline-1").css("transform","translateY(12px) rotate(45deg) ");
+        $(".menuline-2").css("transform","translateY(0px) rotate(-45deg) ");
+        $(".menuline-2").css("width","40px");
+        $(".menuline-3").css("transform","translateY(20px)");
+        
     }
     else{
-        $(".menu").css("transform","translateX(100%)");
         clickmenu=true;
+        $(".menu").css("transform","translateX(110%)");
+        $(".menuline-1").css("transform","translateY(0px) rotate(0deg) ");
+        $(".menuline-2").css("transform","translateY(0px) rotate(0deg) ");
+        $(".menuline-2").css("width","35px");
+        $(".menuline-3").css("transform","translateY(0px)");
     }
     
+}
+
+
+
+//booking_main
+function showcontainer(){
+    $(".select_container").css("transform","translateX(0px)");
+}
+function hidecontainer(){
+    $(".select_container").css("transform","translateX(-850px)");
 }
