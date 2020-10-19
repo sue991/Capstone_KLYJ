e5615bc151d70fa77c8b06b04acbabb661a57a1c
wogns0197 <wogns0197@naver.com>
2020-10-19 21:12:24 +0900

    new design

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ function slideimg(){
     // $(".card").src = 'newimg/hills2.jpg';
     document.getElementById("card12").src = 'newimg/hills2.jpg';
 }
+
+function clicked_menu(a){
+    if(a==1){$(".menu").css("transform","translateY(0)");}
+    else{$(".menu").css("transform","translateY(-100%)");}
+}
+function verify_input(a){
+    var num = ".v" + a.split("_")[1];
+    if(document.getElementById(a).value != ""){        
+        $(num).css("backgroundColor","#A5DF00")
+    }
+    else{
+        $(num).css("backgroundColor","white")
+    }
+}
